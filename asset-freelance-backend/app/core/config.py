a54907c7b2ff539bca001e228cb628eca3013d4f
fahgundin <sfagundes.arthur@gmail.com>
2026-04-25 14:33:08 -0400

    bug fix

--- asset-freelance-backend/app/core/config.py
+++ asset-freelance-backend/app/core/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int = 30
     uploads_dir: str = str(BASE_DIR / "uploads")
     base_url: str = "https://api.aese.com.br"
-''
+
     # SMTP
     smtp_host: str = "smtp.gmail.com"
     smtp_port: int = 587
